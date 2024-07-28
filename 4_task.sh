307aa8c96a2b13001e9b921b53b553a7bc03d3a0
RI <96973065+Rinotrihanna@users.noreply.github.com>
2024-07-28 12:29:54 +0500

    Update 4_task.sh

--- 4_task.sh
+++ 4_task.sh
@@ -1,7 +1,7 @@
 #Четвертое задание.Архивирование и распаковка
 
 
-SOURCE_FOLDER="$(pwd)/MyDirectory1"  #этот папка из 2-го задания
+SOURCE_FOLDER="$(pwd)/MyDirectory1"  #эта папка из 2-го задания
 ARCHIVE_NAME="archive.tar.gz"
 
 tar -czf $ARCHIVE_NAME -C "$(dirname "$SOURCE_FOLDER")" "$(basename "$SOURCE_FOLDER")"
